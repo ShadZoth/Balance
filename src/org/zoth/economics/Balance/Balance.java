dd7f770f79d3d3505873c564773692342511b9af
ShadZoth <megazoth@gmail.com>
2013-10-24 01:05:13 +0400

    Добавлена проверка предусловия

--- src/org/zoth/economics/Balance/Balance.java
+++ src/org/zoth/economics/Balance/Balance.java
@@ -13,6 +13,9 @@ public class Balance {
     }
 
     public static double balance(double Y1, double A, double Cy) {
+        if (Cy >= 1 || Cy <= 0) {
+            throw new IllegalArgumentException();
+        }
         double e = E(Y1, A, Cy);
         if (e != Y1) {
             Y1 = e;
