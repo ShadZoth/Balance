7c258667bbe52d22fb821d63021cf282fcd767af
ShadZoth <megazoth@gmail.com>
2013-10-24 00:39:04 +0400

    Написан код

---
+++ src/org/zoth/economics/Balance/Balance.java
@@ -0,0 +1,24 @@
+package org.zoth.economics.Balance;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author Zoth
+ * @since 23.10.13
+ */
+public class Balance {
+
+    public static double E(double Y, double A, double Cy) {
+        return A + Cy * Y;
+    }
+
+    public static double balance(double Y1, double A, double Cy) {
+        double e = E(Y1, A, Cy);
+        if (e != Y1) {
+            Y1 = e;
+            System.out.println("New Y1: " + Y1);
+            balance(Y1, A, Cy);
+        }
+        return Y1;
+    }
+}
